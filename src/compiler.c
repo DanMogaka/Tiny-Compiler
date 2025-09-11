3fe69552cabda8a8a2d3feb7599ccb1e6d1f8a4d
Dan Mogaka <dannixon360@gmail.com>
2025-09-10 23:40:12 -0500

    Feat Added read_file function

--- src/compiler.c
+++ src/compiler.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function Prototypes
+char *read_file(const char *PATH);
 
 int main(void) {
-    printf("This will be the main driver program for the tiny C program.\n");
+    char *source = read_file("tests/basic.c");
+    if (source) {
+        printf("%s\n", source);
+        free(source);
+    }
     return 0;
+}
+
+// Utility Functions
+char *read_file(const char *PATH) {
+    // Make sure to free dynamic memory with malloc(insert_name)
+    FILE *fp = fopen(PATH, "rb");
+    if (!fp) {
+        perror("fopen");
+        return NULL;
+    }
+    printf("File \'%s\' opened successfully!\n", PATH);
+
+    if (fseek(fp, 0, SEEK_END) != 0) {
+        perror("fseek");
+        fclose(fp);
+        return NULL;
+    }
+
+    long size = ftell(fp);
+    if (size < 0 ) {
+        perror("ftell");
+        fclose(fp);
+        return NULL;
+    }
+    rewind(fp);
+
+    char *buffer = malloc(size + 1);
+    if (!buffer) {
+        perror("malloc");
+        fclose(fp);
+        return NULL;
+    }
+
+    size_t read_size = fread(buffer, 1, size, fp);
+    if (read_size != (size_t)size) {
+        perror("fread");
+        free(buffer);
+        fclose(fp);
+        return NULL;
+    }
+
+    buffer[size] = '\0';
+    fclose(fp);
+    return buffer;
 }
